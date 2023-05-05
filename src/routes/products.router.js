12358f9d6120b08d6029f1570f6676815d9c2256
jmsocorro <jsocorro@sismatica.net>
2023-05-04 22:27:35 -0300

    correccion async endpoints

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -12,69 +12,72 @@ router.get("/", (req, res) => {
         res.status(200).send(prod.getProducts());
     }
 });
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     let { limit } = req.query;
-    if (limit) {
-        res.status(200).send(prod.getProducts().slice(0, +limit));
-    } else {
-        res.status(200).send(prod.getProducts());
+    try {
+        const productos = await prod.getProducts();
+        if (limit) {
+            res.status(200).send(productos.slice(0, +limit));
+        } else {
+            res.status(200).send(productos);
+        }
+    } catch (err) {
+        res.status(400).send(err);
     }
 });
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
     let id = parseInt(req.params.id);
-    let foundprod = prod.getProductById(id);
-    if (foundprod === false) {
-        res.status(404).send({ error: "Producto no encontrado" });
-    } else {
-        res.status(200).send(foundprod);
+    try {
+        const foundprod = await prod.getProductById(id);
+        if (foundprod === false) {
+            res.status(404).send({ error: "Producto no encontrado" });
+        } else {
+            res.status(200).send(foundprod);
+        }
+    } catch (err) {
+        res.status(400).send(err);
     }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     const producto = req.body;
-    (async () => {
-        try {
-            const result = await prod.addProduct(producto);
-            if (result.error) {
-                res.status(400).send(result);
-            } else {
-                res.status(201).send(result);
-            }
-        } catch (err) {
-            res.status(400).send(err);
+    try {
+        const result = await prod.addProduct(producto);
+        if (result.error) {
+            res.status(400).send(result);
+        } else {
+            res.status(201).send(result);
         }
-    })();
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
-router.put("/", (req, res) => {
+router.put("/", async (req, res) => {
     const producto = req.body;
-    (async () => {
-        try {
-            const result = await prod.updateProduct(producto);
-            if (result.error) {
-                res.status(400).send(result);
-            } else {
-                res.status(200).send(result);
-            }
-        } catch (err) {
-            res.status(400).send(err);
+    try {
+        const result = await prod.updateProduct(producto);
+        if (result.error) {
+            res.status(400).send(result);
+        } else {
+            res.status(200).send(result);
         }
-    })();
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
     let id = parseInt(req.params.id);
-    (async () => {
-        try {
-            const result = await prod.deleteProduct(id);
-            if (result.error) {
-                res.status(400).send(result);
-            } else {
-                res.status(204);
-            }
-        } catch (err) {
-            res.status(400).send(err);
+    try {
+        const result = await prod.deleteProduct(id);
+        if (result.error) {
+            res.status(400).send(result);
+        } else {
+            res.status(200).send(result);
         }
-    })();
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 
 export default router;
